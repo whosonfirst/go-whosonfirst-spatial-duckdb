f1279933bd80f30f01d1f0c9ba6ef499cedbc92b
sfomuseumbot <sfomuseumbot@localhost>
2024-12-11 18:07:53 -0800

    fleshing out code

--- database.go
+++ database.go
@@ -4,6 +4,8 @@ import (
 	"context"
 	"database/sql"
 	"fmt"
+	"iter"
+	"net/url"
 
 	"github.com/paulmach/orb"
 	"github.com/whosonfirst/go-whosonfirst-spatial"
@@ -13,11 +15,51 @@ import (
 
 type DuckDBSpatialDatabase struct {
 	database.SpatialDatabase
-	conn *sql.DB
+	conn         *sql.DB
+	database_uri string
 }
 
 func NewDuckDBSpatialDatabase(ctx context.Context, uri string) (database.SpatialDatabase, error) {
-	return nil, fmt.Errorf("Not implemented")
+
+	_, err := url.Parse(uri)
+
+	if err != nil {
+		return nil, err
+	}
+
+	conn, err := sql.Open("duckdb", "")
+
+	if err != nil {
+		return nil, fmt.Errorf("Failed to create database connection, %w", err)
+	}
+
+	extensions := []string{
+		"spatial",
+	}
+
+	for _, ext := range extensions {
+
+		cmds := []string{
+			fmt.Sprintf("INSTALL %s", ext),
+			fmt.Sprintf("LOAD %s", ext),
+		}
+
+		for _, q := range cmds {
+
+			_, err := conn.ExecContext(ctx, q)
+
+			if err != nil {
+				return nil, fmt.Errorf("Failed to configure data - query failed, %w (%s)", err, q)
+			}
+		}
+	}
+
+	db := &DuckDBSpatialDatabase{
+		conn:         conn,
+		database_uri: uri,
+	}
+
+	return db, nil
 }
 
 func (db *DuckDBSpatialDatabase) IndexFeature(context.Context, []byte) error {
@@ -30,9 +72,18 @@ func (db *DuckDBSpatialDatabase) RemoveFeature(context.Context, string) error {
 
 func (db *DuckDBSpatialDatabase) PointInPolygon(ctx context.Context, coord *orb.Point, filters ...spatial.Filter) (spr.StandardPlacesResults, error) {
 
-	return nil, fmt.Errorf("Not implemented.")
+	results := make([]spr.StandardPlacesResult, 0)
 
-	// SELECT id,name FROM read_parquet('/usr/local/whosonfirst-data-admin-latest.parquet') WHERE ST_Contains(geometry::GEOMETRY, 'POINT(-113.49058382120987 53.55192911144368)'::GEOMETRY);
+	for spr, err := range db.pointInPolygon(ctx, coord, filters...) {
+
+		if err != nil {
+			return nil, err
+		}
+
+		results = append(results, spr)
+	}
+
+	return nil, fmt.Errorf("Not implemented.")
 }
 
 func (db *DuckDBSpatialDatabase) PointInPolygonCandidates(ctx context.Context, centroid *orb.Point, filters ...spatial.Filter) ([]*spatial.PointInPolygonCandidate, error) {
@@ -40,8 +91,21 @@ func (db *DuckDBSpatialDatabase) PointInPolygonCandidates(ctx context.Context, c
 	return nil, fmt.Errorf("Not implemented.")
 }
 
-func (db *DuckDBSpatialDatabase) PointInPolygonWithChannels(ctx context.Context, spr_ch chan spr.StandardPlacesResult, err_ch chan error, done_ch chan bool, pt *orb.Point, filters ...spatial.Filter) {
+func (db *DuckDBSpatialDatabase) PointInPolygonWithChannels(ctx context.Context, spr_ch chan spr.StandardPlacesResult, err_ch chan error, done_ch chan bool, coord *orb.Point, filters ...spatial.Filter) {
 
+	defer func() {
+		done_ch <- true
+	}()
+
+	for spr, err := range db.pointInPolygon(ctx, coord, filters...) {
+
+		if err != nil {
+			err_ch <- err
+			break
+		}
+
+		spr_ch <- spr
+	}
 }
 
 func (db *DuckDBSpatialDatabase) PointInPolygonCandidatesWithChannels(ctx context.Context, candidate_ch chan *spatial.PointInPolygonCandidate, err_ch chan error, done_ch chan bool, pt *orb.Point, filters ...spatial.Filter) {
@@ -50,4 +114,45 @@ func (db *DuckDBSpatialDatabase) PointInPolygonCandidatesWithChannels(ctx contex
 
 func (db *DuckDBSpatialDatabase) Disconnect(ctx context.Context) error {
 	return db.conn.Close()
+}
+
+func (db *DuckDBSpatialDatabase) pointInPolygon(ctx context.Context, coord *orb.Point, filters ...spatial.Filter) iter.Seq2[spr.StandardPlacesResult, error] {
+
+	return func(yield func(spr.StandardPlacesResult, error) bool) {
+
+		yield(nil, fmt.Errorf("Not implemented."))
+
+		q := "SELECT id FROM read_parquet('?') WHERE ST_Contains(geometry::GEOMETRY, 'POINT(? ?)'::GEOMETRY)"
+
+		rows, err := db.conn.QueryContext(ctx, q, coord.X, coord.Y)
+
+		if err != nil {
+			yield(nil, err)
+			return
+		}
+
+		defer rows.Close()
+
+		for rows.Next() {
+
+			var id int64
+
+			err := rows.Scan(&id)
+
+			if err != nil {
+				yield(nil, err)
+				break
+			}
+
+			// Derive SPR here...
+		}
+
+		err = rows.Close()
+
+		if err != nil {
+			yield(nil, err)
+		}
+	}
+
+	//
 }
